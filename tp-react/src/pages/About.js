94e19f7a75c6231566b2ad8e2f0ea26de2e8a685
idryssj31 <idryss3108@gmail.com>
2025-04-24 21:28:23 +0200

    [UPD] : NavLink

---
+++ tp-react/src/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <p>blablabla</p>
+    </div>
+  );
+};
+
+export default About;
